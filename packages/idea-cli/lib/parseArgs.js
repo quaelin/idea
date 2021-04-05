f6f577c2d9c0e636bd7b435c34eb40bdc5fc4532
chris <christopher.d.campbell@gmail.com>
2021-04-05 11:34:18 -0700

    @quaelin/idea-cli@0.0.3

--- packages/idea-cli/lib/parseArgs.js
+++ packages/idea-cli/lib/parseArgs.js
@@ -1,12 +1,9 @@
 const minimist = require('minimist');
 
-module.exports = ({ argv /* , env */ }) => {
+module.exports = ({ argv, env }) => {
   const args = minimist(argv.slice(2));
 
-  args.ipfsConfig = {
-    http: 'http://localhost:5001/api/v0',
-    ...process.env.IDEA_IPFS_HTTP,
-  };
+  args.ipfsConfig = { http: env.IDEA_IPFS_HTTP || 'http://localhost:5001/api/v0' };
 
   return args;
 };
