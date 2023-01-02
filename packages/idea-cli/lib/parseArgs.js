ac51afa5ef657a497eb4277d88a4d19a2106f302
Chris Campbell <christopher.d.campbell@gmail.com>
2023-01-01 17:30:33 -0800

    Updates

--- packages/idea-cli/lib/parseArgs.js
+++ packages/idea-cli/lib/parseArgs.js
@@ -3,7 +3,7 @@ import minimist from 'minimist';
 export function parseArgs({ argv, env }) {
   const args = minimist(argv.slice(2));
 
-  args.ipfsConfig = { http: env.IDEA_IPFS_HTTP || 'http://localhost:5001/api/v0' };
+  args.ipfsConfig = { http: env.IDEA_IPFS_HTTP || 'http://127.0.0.1:5001/api/v0' };
 
   return args;
 }
