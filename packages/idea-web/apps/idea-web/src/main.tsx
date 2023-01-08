d0b6a065da4025b7b55a9627d4361d448db24160
Chris Campbell <christopher.d.campbell@gmail.com>
2023-01-08 15:24:33 -0800

    Disable StrictMode so as to fix drag n drop

--- packages/idea-web/apps/idea-web/src/main.tsx
+++ packages/idea-web/apps/idea-web/src/main.tsx
@@ -5,8 +5,12 @@ import App from './app/app';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
+// Note: Had to disable StrictMode here because react-beautiful-dnd doesn't yet
+// work properly with it under React 18, see
+// https://github.com/atlassian/react-beautiful-dnd/issues/2350
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <App />
-  </React.StrictMode>
+  //</React.StrictMode>
 );
